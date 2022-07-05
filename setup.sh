8104a292d43117b7c77630b3aa4ac37d7ac64d68
subhasish4x <108706305+subhasish4x@users.noreply.github.com>
2022-07-05 02:30:34 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,50 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-## Author  : Aditya Shakya (adi1090x)
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-## Termux Desktop : Setup GUI in Termux 
-
-## ANSI Colors (FG & BG)
-RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
-MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
-REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
-MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
-
-## Reset terminal colors
-reset_color() {
-	printf '\033[37m'
-}
-
-## Script Termination
-exit_on_signal_SIGINT() {
-    { printf "${RED}\n\n%s\n\n" "[!] Program Interrupted." 2>&1; reset_color; }
-    exit 0
-}
-
-exit_on_signal_SIGTERM() {
-    { printf "${RED}\n\n%s\n\n" "[!] Program Terminated." 2>&1; reset_color; }
-    exit 0
-}
-
-trap exit_on_signal_SIGINT SIGINT
-trap exit_on_signal_SIGTERM SIGTERM
-
-## Banner
-banner() {
-	clear
-    cat <<- EOF
-		${RED}┌──────────────────────────────────────────────────────────┐
-		${RED}│${GREEN}░░░▀█▀░█▀▀░█▀▄░█▄█░█░█░█░█░░░█▀▄░█▀▀░█▀▀░█░█░▀█▀░█▀█░█▀█░░${RED}│
-		${RED}│${GREEN}░░░░█░░█▀▀░█▀▄░█░█░█░█░▄▀▄░░░█░█░█▀▀░▀▀█░█▀▄░░█░░█░█░█▀▀░░${RED}│
-		${RED}│${GREEN}░░░░▀░░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░░░▀▀░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░░░░${RED}│
-		${RED}└──────────────────────────────────────────────────────────┘
-		${BLUE}By : Aditya Shakya // @adi1090x
-	EOF
-}
-
 ## Show usages
 usage() {
 	banner
@@ -53,11 +7,11 @@ usage() {
 }
 
 ## Update, X11-repo, Program Installation
-_pkgs=(bc bmon calc calcurse curl dbus desktop-file-utils elinks feh fontconfig-utils fsmon \
-		geany git gtk2 gtk3 htop-legacy imagemagick jq leafpad man mpc mpd mutt ncmpcpp \
-		ncurses-utils neofetch netsurf obconf openbox openssl-tool polybar ranger rofi \
-		startup-notification termux-api thunar tigervnc vim wget xarchiver xbitmaps xcompmgr \
-		xfce4-settings xfce4-terminal xmlstarlet xorg-font-util xorg-xrdb zsh)
+_pkgs=(desktop-file-utils feh fontconfig-utils \
+		geany git gtk2 gtk3 ncmpcpp \
+		ncurses-utils obconf openbox rofi \
+		thunar tigervnc neovim xbitmaps xcompmgr \
+		xfce4-settings xfce4-terminal xmlstarlet xorg-font-util xorg-xrdb )
 
 setup_base() {
 	echo -e ${RED}"\n[*] Installing Termux Desktop..."
@@ -80,113 +34,6 @@ setup_base() {
 	done
 	reset_color
 }
-
-## Setup OMZ and Termux Configs
-setup_omz() {
-	# backup previous termux and omz files
-	echo -e ${RED}"[*] Setting up OMZ and termux configs..."
-	omz_files=(.oh-my-zsh .termux .zshrc)
-	for file in "${omz_files[@]}"; do
-		echo -e ${CYAN}"\n[*] Backing up $file..."
-		if [[ -f "$HOME/$file" || -d "$HOME/$file" ]]; then
-			{ reset_color; mv -u ${HOME}/${file}{,.old}; }
-		else
-			echo -e ${MAGENTA}"\n[!] $file Doesn't Exist."			
-		fi
-	done
-	# installing omz
-	echo -e ${CYAN}"\n[*] Installing Oh-my-zsh... \n"
-	{ reset_color; git clone https://github.com/robbyrussell/oh-my-zsh.git --depth 1 $HOME/.oh-my-zsh; }
-	cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
-	sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="aditya"/g' $HOME/.zshrc
-	sed -i -e 's|# export PATH=.*|export PATH=$HOME/.local/bin:$PATH|g' $HOME/.zshrc
-	# ZSH theme
-	cat > $HOME/.oh-my-zsh/custom/themes/aditya.zsh-theme <<- _EOF_
-		# Default OMZ theme
-
-		if [[ "\$USER" == "root" ]]; then
-		  PROMPT="%(?:%{\$fg_bold[red]%}%{\$fg_bold[yellow]%}%{\$fg_bold[red]%} :%{\$fg_bold[red]%} )"
-		  PROMPT+='%{\$fg[cyan]%}  %c%{\$reset_color%} \$(git_prompt_info)'
-		else
-		  PROMPT="%(?:%{\$fg_bold[red]%}%{\$fg_bold[green]%}%{\$fg_bold[yellow]%} :%{\$fg_bold[red]%} )"
-		  PROMPT+='%{\$fg[cyan]%}  %c%{\$reset_color%} \$(git_prompt_info)'
-		fi
-
-		ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[blue]%}  git:(%{\$fg[red]%}"
-		ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
-		ZSH_THEME_GIT_PROMPT_DIRTY="%{\$fg[blue]%}) %{\$fg[yellow]%}✗"
-		ZSH_THEME_GIT_PROMPT_CLEAN="%{\$fg[blue]%})"
-	_EOF_
-	# Append some aliases
-	cat >> $HOME/.zshrc <<- _EOF_
-		#------------------------------------------
-		alias l='ls -lh'
-		alias ll='ls -lah'
-		alias la='ls -a'
-		alias ld='ls -lhd'
-		alias p='pwd'
-
-		#alias rm='rm -rf'
-		alias u='cd $PREFIX'
-		alias h='cd $HOME'
-		alias :q='exit'
-		alias grep='grep --color=auto'
-		alias open='termux-open'
-		alias lc='lolcat'
-		alias xx='chmod +x'
-		alias rel='termux-reload-settings'
-
-		#------------------------------------------
-
-		# SSH Server Connections
-
-		# linux (Arch)
-		#alias arch='ssh UNAME@IP -i ~/.ssh/id_rsa.DEVICE'
-
-		# linux sftp (Arch)
-		#alias archfs='sftp -i ~/.ssh/id_rsa.DEVICE UNAME@IP'
-	_EOF_
-
-	# configuring termux
-	echo -e ${CYAN}"\n[*] Configuring Termux..."
-	if [[ ! -d "$HOME/.termux" ]]; then
-		mkdir $HOME/.termux
-	fi
-	# copy font
-	cp $(pwd)/files/.fonts/icons/dejavu-nerd-font.ttf $HOME/.termux/font.ttf
-	# color-scheme
-	cat > $HOME/.termux/colors.properties <<- _EOF_
-		background 		: #263238
-		foreground 		: #eceff1
-
-		color0  			: #263238
-		color8  			: #37474f
-		color1  			: #ff9800
-		color9  			: #ffa74d
-		color2  			: #8bc34a
-		color10 			: #9ccc65
-		color3  			: #ffc107
-		color11 			: #ffa000
-		color4  			: #03a9f4
-		color12 			: #81d4fa
-		color5  			: #e91e63
-		color13 			: #ad1457
-		color6  			: #009688
-		color14 			: #26a69a
-		color7  			: #cfd8dc
-		color15 			: #eceff1
-	_EOF_
-	# button config
-	cat > $HOME/.termux/termux.properties <<- _EOF_
-		extra-keys = [ \\
-		 ['ESC','|', '/', '~','HOME','UP','END'], \\
-		 ['CTRL', 'TAB', '=', '-','LEFT','DOWN','RIGHT'] \\
-		]	
-	_EOF_
-	# change shell and reload configs
-	{ chsh -s zsh; termux-reload-settings; termux-setup-storage; }
-}
-
 ## Configuration
 setup_config() {
 	# backup
@@ -286,10 +133,8 @@ post_msg() {
 install_td() {
 	banner
 	setup_base
-	setup_omz
 	setup_config
 	setup_vnc
-	setup_launcher
 	post_msg
 }
 
